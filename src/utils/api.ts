580153f08700e595b87dc458bd32e8c1f252a1cf
doTryCatch <rpsuper1640@gmail.com>
2025-11-13 22:27:42 +0530

    fixed:url error 2

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://rate-my-store-backend.onrender.com/api",
+  baseURL:
+    `${process.env.REACT_APP_API_URL}` ||
+    "https://rate-my-store-backend.onrender.com/api",
   withCredentials: true,
 });
