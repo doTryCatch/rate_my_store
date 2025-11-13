bbc2a4e448bdb2ab3fe7536054a14272372fccdc
doTryCatch <rpsuper1640@gmail.com>
2025-11-13 22:02:50 +0530

    added:api url

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: `${process.env.REACT_APP_API_URL}`,
+  baseURL:
+    `${process.env.REACT_APP_API_URL}` ||
+    "https://rate-my-store-backend.onrender.com/api",
   withCredentials: true,
 });
