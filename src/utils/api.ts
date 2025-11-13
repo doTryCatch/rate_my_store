8e42edafd1a2b70c326f390bf6ac97e39cd9be72
doTryCatch <rpsuper1640@gmail.com>
2025-11-14 00:12:35 +0530

    changed:api point

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const api = axios.create({
   baseURL:
     `${process.env.REACT_APP_API_URL}` ||
-    "https://rate-my-store-backend.onrender.com/api",
+    "https://rate-my-store-backend-oclt.vercel.app",
   withCredentials: true,
 });
